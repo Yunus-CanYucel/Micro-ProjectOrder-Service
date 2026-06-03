d9a0e502f5358f698c9af88df3451ca428d8f417
Yunus-CanYucel <yunuscan.ycel@iths.se>
2026-06-03 10:41:46 +0200

    added to application + redone TestRabbitMQConfig

--- src/test/java/se/iths/yunuscan/microprojectorderservice/config/TestRabbitMQConfig.java
+++ src/test/java/se/iths/yunuscan/microprojectorderservice/config/TestRabbitMQConfig.java
@@ -1,34 +1,25 @@
 package se.iths.yunuscan.microprojectorderservice.config;
 
 
+import org.mockito.Mockito;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
-import org.springframework.amqp.core.Queue;
+
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.amqp.support.converter.JacksonJsonMessageConverter;
-import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+
 
 @TestConfiguration
 public class TestRabbitMQConfig {
-
     @Bean
     @Primary
-    public Queue orderEmailQueue() {
-        return new Queue("test-queue", true);
+    public RabbitTemplate rabbitTemplate() {
+        return Mockito.mock(RabbitTemplate.class);
     }
-
     @Bean
     @Primary
-    public JacksonJsonMessageConverter messageConverter() {
-        return new JacksonJsonMessageConverter();
-    }
-
-    @Bean
-    @Primary
-    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
-        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
-        rabbitTemplate.setMessageConverter(messageConverter());
-        return rabbitTemplate;
+    public RabbitAdmin rabbitAdmin() {
+        return Mockito.mock(RabbitAdmin.class);
     }
 }
